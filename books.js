f18fa1a6a3abde55d3542ec64c16551b281f30f0
madisenegan <eganm3@uw.edu>
2018-05-31 12:42:40 -0500

    Final Project

---
+++ books.js
@@ -0,0 +1,92 @@
+const books = [
+    {
+        "title" : "The Power",
+        "author" : "Naomi Alderman",
+        "cover" : "https://images-na.ssl-images-amazon.com/images/I/51PUiZ2CfqL._SX329_BO1,204,203,200_.jpg",
+    },
+    {
+        "title" : "Rooms",
+        "author" : "Lauren Oliver",
+        "cover" : "https://images.gr-assets.com/books/1429900056l/13579626.jpg",
+    },
+    {
+        "title" : "Severance",
+        "author" : "Ling Ma",
+        "cover" : "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjKjOCZv7DbAhWK5YMKHT1RD9QQjRx6BAgBEAU&url=https%3A%2F%2Fwww.amazon.com%2FSeverance-Novel-Ling-Ma%2Fdp%2F0374261598&psig=AOvVaw1lkVCjuOqy67KRjR0me-Qs&ust=1527874770088031",
+    },
+    {
+        "title" : "The Outsider",
+        "author" : "Stephen King",
+        "cover" : "https://prodimage.images-bn.com/pimages/9781501180989_p0_v4_s550x406.jpg",
+    },
+    {
+        "title" : "Early Work",
+        "author" : "Andrew Martin",
+        "cover" : "https://images.macmillan.com/folio-assets/macmillan_us_frontbookcovers_1000H/9780374146122.jpg",
+    },
+    {
+        "title" : "Still Lives",
+        "author" : "Maria Hummel",
+        "cover" : "https://images-na.ssl-images-amazon.com/images/I/81qn-HFyLYL.jpg",
+    },
+    {
+        "title" : "The Incendiaries",
+        "author" : "R.O. Kwan",
+        "cover" : "https://images-na.ssl-images-amazon.com/images/I/515lMGuNGQL._SX316_BO1,204,203,200_.jpg",
+    },
+    {
+        "title" : "Undermajordomo Minor",
+        "author" : "Patrick Dewitt",
+        "cover" : "https://images-na.ssl-images-amazon.com/images/I/51B0a-5M%2B0L._SX330_BO1,204,203,200_.jpg",
+    },
+    {
+        "title" : "Little Nothing",
+        "author" : "Marisa Silver",
+        "cover" : "https://images.gr-assets.com/books/1500552629l/33931113.jpg",
+    },
+    {
+        "title" : "The Magic Toyshop",
+        "author" : "Angela Carter",
+        "cover" : "https://images.gr-assets.com/books/1388410534l/81026.jpg",
+    },
+    {
+        "title" : "Mr. Fox",
+        "author" : "Helen Oyeyemi",
+        "cover" : "",
+    },
+    {
+        "title" : "The Immortalists",
+        "author" : "Chloe Benjamin",
+        "cover" : "",
+    },
+    {
+        "title" : "The Year of the Flood",
+        "author" : "Margaret Atwood",
+        "cover" : "",
+    },
+    {
+        "title" : "The Left Hand of Darkness",
+        "author" : "Ursula K. Le Guin",
+        "cover" : "",
+    },
+    {
+        "title" : "Red Clocks",
+        "author" : "Leni Zumas",
+        "cover" : "",
+    },
+    {
+        "title" : "Hollow",
+        "author" : "Owen Egerton",
+        "cover" : "",
+    },
+    {
+        "title" : "Lotus",
+        "author" : "Lijia Zhang",
+        "cover" : "",
+    },
+    {
+        "title" : "Sunshine State",
+        "author" : "Sarah Gerard",
+        "cover" : "",
+    },
+]
---
+++ movies.js
@@ -0,0 +1,10 @@
+const movies = [
+    {
+        "title" : "Solo: A Star Wars Movie",
+        "rating" : 3,
+        "year" : 2018,
+    }
+    {}
+    
+]
+
