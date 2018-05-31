76f94656668254bc3326529617b254030241ea5f
madisenegan <37811184+madisenegan@users.noreply.github.com>
2018-05-31 13:52:21 -0500

    Update movies.js

--- movies.js
+++ movies.js
@@ -1,10 +1,30 @@
 const movies = [
     {
-        "title" : "Solo: A Star Wars Movie",
-        "rating" : 3,
-        "year" : 2018,
-    }
-    {}
+        "title" : "Good Time",
+        "director: "The Safide Brothers",
+        "poster" : "https://ia.media-imdb.com/images/M/MV5BNGQ3Y2VjNmUtMDdiMC00YTE1LWI5NmUtZDIzMTJmODQyNDU4XkEyXkFqcGdeQXVyNTAzMTY4MDA@._V1_SY1000_CR0,0,675,1000_AL_.jpg",
+    },
+        {
+        "title" : "Song to Song",
+        "director: "Terrence Malick",
+        "poster" : "https://ia.media-imdb.com/images/M/MV5BNzI1MTg0MzEyOF5BMl5BanBnXkFtZTgwNzk4Mjk0MTI@._V1_SY1000_CR0,0,674,1000_AL_.jpg",
+    },
+        {
+        "title" : "American Made",
+        "director: "Doug Liman",
+        "poster" : "https://ia.media-imdb.com/images/M/MV5BMTUxNzUwMjk1Nl5BMl5BanBnXkFtZTgwNDkwODI1MjI@._V1_UX182_CR0,0,182,268_AL_.jpg",
+    },
+        {
+        "title" : "",
+        "director: "",
+        "poster" : " ",
+    },
+        {
+        "title" : "",
+        "director: "",
+        "poster" : " ",
+    },
+    
     
 ]
 
